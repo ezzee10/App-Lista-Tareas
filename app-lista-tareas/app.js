59fdee1492df9f2c938227c2bb94fecf4c1b8f78
Ezequiel Colombano <ecolombano@proveedor.teco.com.ar>
2021-07-26 19:41:39 -0300

    Se realizó ejercicio 3

--- app-lista-tareas/app.js
+++ app-lista-tareas/app.js
@@ -1,9 +1,20 @@
 const {mostrarMenu, pausa} = require('../helpers/mensajes')
-console.clear()
 
 const main = async () => {
-  mostrarMenu();
-  //pausa();
+
+  let opt = '';
+
+  do {
+    try {
+      opt = await mostrarMenu();
+    }catch (e) {
+      console.log(e);
+      opt = '';
+    }
+    console.log({opt});
+    await pausa();
+  } while ( opt !== '0');
 };
 
 main();
+
--- helpers/mensajes.js
+++ helpers/mensajes.js
@@ -1,43 +1,48 @@
-const colors = require('colors')
+const colors = require('colors');
 const readline = require('readline');
 
 const mostrarMenu = () => {
+    console.clear();
+    return new Promise((resolve, reject) => {
+        console.log(colors.red('************************** \n   Seleccione una opción \n************************** \n'))
+        console.log(colors.yellow('¿Qué desea hacer?'))
+        console.log(colors.green('1.'), 'Crear tarea')
+        console.log(colors.green('2.'), 'Listar tareas')
+        console.log(colors.green('3.'), 'Listar tareas completadas')
+        console.log(colors.green('4.'), 'Listar tareas pendientes')
+        console.log(colors.green('5.'), 'Completar tarea(s)')
+        console.log(colors.green('6.'), 'Borrar tarea')
+        console.log(colors.green('0.'), 'Salir')
 
-    console.log(colors.red('************************** \n   Seleccione una opción \n************************** \n'))
-    console.log(colors.yellow('¿Qué desea hacer?'))
-    console.log(colors.green('1.'), 'Crear tarea')
-    console.log(colors.green('2.'), 'Listar tareas')
-    console.log(colors.green('3.'), 'Listar tareas completadas')
-    console.log(colors.green('4.'), 'Listar tareas pendientes')
-    console.log(colors.green('5.'), 'Completar tarea(s)')
-    console.log(colors.green('6.'), 'Borrar tarea')
-    console.log(colors.green('0.'), 'Salir')
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
 
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-
-    rl.question('Seleccione una opción: ', (opt) => {
-        
-        console.log(`{ opt: ${opt} }`);
-      
-        rl.close();
+        rl.question('Seleccione una opción: ', (opt) => {
+            if (opt < 0 || opt > 6 || isNaN(opt) || opt === '') {
+                reject('La opción debe ser un número entero entre 0 y 6');
+            }
+            resolve(opt);
+            rl.close();
+        });
     });
 }
 
-
 const pausa = () => {
 
-    const rl = require('readline').createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
+    return new Promise((resolve, reject) => {
 
-    rl.question(`Presione ${colors.blue('ENTER')} para continuar `, (option) => {
-        rl.close();
+        const rl = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+    
+        rl.question(`Presione ${colors.blue('ENTER')} para continuar `, (opt) => {
+            rl.close();
+	        resolve();
+        });
     });
-
 }
 
 
